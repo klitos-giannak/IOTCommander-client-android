738af47e281d4d8d136961bfe8ef63b6f06534be
Klitos Giannakopoulos <klitos@duckseason.mobi>
2021-12-22 21:53:36 +0200

    Change theme colours

--- app/src/main/java/mobi/duckseason/iotcommander/ui/theme/Color.kt
+++ app/src/main/java/mobi/duckseason/iotcommander/ui/theme/Color.kt
@@ -2,9 +2,8 @@ package mobi.duckseason.iotcommander.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
 val PaleBlue = Color(0xffb0ddfb)
 val DarkPaleBlue = Color(0xff002d4b)
+val Orange = Color(0xffeb6e00)
+val OrangeVariant = Color(0xffd75a00)
+val DarkYellow = Color(0xfff5bc11)
--- app/src/main/java/mobi/duckseason/iotcommander/ui/theme/Theme.kt
+++ app/src/main/java/mobi/duckseason/iotcommander/ui/theme/Theme.kt
@@ -7,16 +7,16 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200,
+    primary = Orange,
+    primaryVariant = OrangeVariant,
+    secondary = DarkYellow,
     surface = DarkPaleBlue
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200,
+    primary = Orange,
+    primaryVariant = OrangeVariant,
+    secondary = DarkYellow,
     surface = PaleBlue
 
     /* Other default colors to override
